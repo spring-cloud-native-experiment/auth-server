9b660f87cb38738418a6e6bc5b0a32498055a144
Praveer Gupta <praveerg@inpraveerg.local>
2016-09-14 23:54:01 +0530

    Add JWT Support to Auth Server

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # auth-server
+
+
+To generate key-pair use the below command.
+
+```
+keytool -genkeypair -alias mytest -keyalg RSA -keypass mypass -keystore mytest.jks -storepass mypass
+```
+
+
+To get the public key.
+ 
+```
+keytool -list -rfc --keystore mytest.jks | openssl x509 -inform pem -pubkey
+```
+
+##References
+[Spring-security OAuth JWT tutorial on Baeldung](http://www.baeldung.com/spring-security-oauth-jwt)  
+[JWT OAuth tutorial on Spring Guides](https://github.com/spring-guides/tut-spring-security-and-angular-js/blob/master/oauth2/authserver/src/main/java/demo/AuthserverApplication.java)
--- src/main/java/com/example/AuthServiceApplication.java
+++ src/main/java/com/example/AuthServiceApplication.java
@@ -1,20 +1,30 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.security.KeyPair;
 import java.security.Principal;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 @EnableResourceServer
 @RestController
 @EnableEurekaClient
@@ -27,5 +37,43 @@ public class AuthServiceApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+        @SuppressWarnings("SpringJavaAutowiringInspection")
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        public JwtAccessTokenConverter jwtAccessTokenConverter() {
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            KeyPair keyPair = new KeyStoreKeyFactory(
+                    new ClassPathResource("mytest.jks"),
+                    "mypass".toCharArray()
+            ).getKeyPair("mytest");
+            converter.setKeyPair(keyPair);
+            return converter;
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("acme").secret("acmesecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password")
+                    .scopes("openid").autoApprove("openid");
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+        }
     }
 }
